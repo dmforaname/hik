0abdf2ca3e3e0f97f82d031bcb2ddd0d6df34c55
Widiasa Mahendra <dmforaname@gmail.com>
2021-11-13 16:22:34 +0700

    user roles model migration

--- app/User.php
+++ app/User.php
@@ -36,4 +36,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function Roles()
+    {
+        return $this->belongsToMany('App\Models\Role', 'user_roles', 'user_id', 'role_id');
+    }
 }
